84629ad453a848b08dbb10ca5d212388bcf0b5d6
Nizural <christophe.sensebe@gmail.com>
2014-08-28 23:27:53 +0200

    testing...

--- game.js
+++ game.js
@@ -5,6 +5,32 @@ var imageRepository = new function(){
 }
 
 function draw(){
-	var ctx  = document.getElementById("gameCanvas").getContext("2d");
+	var ctx = document.getElementById("gameCanvas").getContext("2d");
 	ctx.drawImage(imageRepository.background1, 0, 0);
+}
+
+//base class for drawable objects
+function Drawable(){
+	this.init = function(x,y){
+		this.x = x;
+		this.y = y;
+	}
+	this.speed = 0;
+	this.canvasWidth = 0;
+	this.canvasHeight = 0;
+}
+
+//background object
+function Background(){
+	this.speed = 1;
+	this.draw = function(){
+		this.ctx = document.getElementById("gameCanvas").getContext("2d");
+		ctx.drawImage(imageRepository.background1, 0, 0);
+	}
+}
+
+Background.prototype = new Drawable();
+
+function game(){
+	
 }
