8e2a7a90d2051ef2d9da9aaed7f8529862cbef6c
Nizural <christophe.sensebe@gmail.com>
2014-08-28 23:08:17 +0200

    function draw() #2

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
+var imageRepository = new function(){
+	//background1
+	this.background1 = new Image();
+	this.background1.src = "background1.png";
+}
+
 function draw(){
-	var ctx = document.getElementById("gameCanvas").getContext("2d");
-	var img = new Image();
-	img.src = "background1.png";
-	ctx.drawImage(img,0,0);
+	var ctx  = document.getElementById("gameCanvas").getContext("2d");
+	ctx.drawImage(imageRepository.background1, 0, 0);
 }
