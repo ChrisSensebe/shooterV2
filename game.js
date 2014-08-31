bdc55ea9f655b1a1023a16a74c9e32b47971315f
Nizural <christophe.sensebe@gmail.com>
2014-08-31 15:48:02 +0200

    drawing on canvas works

--- game.js
+++ game.js
@@ -25,12 +25,14 @@ function Background(){
 	this.speed = 1;
 	this.draw = function(){
 		this.ctx = document.getElementById("gameCanvas").getContext("2d");
-		ctx.drawImage(imageRepository.background1, 0, 0);
+		this.ctx.drawImage(imageRepository.background1, this.x, this.y);
 	}
 }
 
 Background.prototype = new Drawable();
 
 function game(){
-	
+	this.background1 = new Background();
+	this.background1.init(0,0);
+	this.background1.draw();
 }
